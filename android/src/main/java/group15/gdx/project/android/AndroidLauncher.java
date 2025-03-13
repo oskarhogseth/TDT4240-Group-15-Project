873a9d2173db108ab2d3d92a32ae2955141ed906
Oskar Høgseth <oskarhogseth01@gmail.com>
2025-03-13 13:44:27 +0100

    Created project for group 15

---
+++ android/src/main/java/group15/gdx/project/android/AndroidLauncher.java
@@ -0,0 +1,18 @@
+package group15.gdx.project.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import group15.gdx.project.Launcher;
+
+/** Launches the Android application. */
+public class AndroidLauncher extends AndroidApplication {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
+        configuration.useImmersiveMode = true; // Recommended, but not required.
+        initialize(new Launcher(), configuration);
+    }
+}
