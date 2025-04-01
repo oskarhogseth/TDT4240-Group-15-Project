4f6b7bfa6757277252fbcc6ac4157ee913b6da5e
oskarhogseth <115374602+oskarhogseth@users.noreply.github.com>
2025-04-01 10:47:04 +0200

    Update GameLobby.java

--- core/src/main/java/group15/gdx/project/model/GameLobby.java
+++ core/src/main/java/group15/gdx/project/model/GameLobby.java
@@ -4,12 +4,30 @@ import static com.badlogic.gdx.math.MathUtils.random;
 // Kilde: https://stackoverflow.com/questions/33847225/generating-a-random-pin-of-5-digits
 
 import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.List;
 
 public class GameLobby {
-    private final String gamePin;
+    //private final String gamePin;
+    private List<Player> players;
+
     public GameLobby() {
-        this.gamePin = generateRandomPin();
+        // Properly initialize the players list
+        players = new ArrayList<>();
     }
+
+    public void addPlayer(Player player) {
+        players.add(player);
+    }
+
+    public List<Player> getPlayers() {
+        return players;
+    }
+
+
+    //public GameLobby() {
+    //    this.gamePin = generateRandomPin();
+    //}
 
     @SuppressWarnings("DefaultLocale")
     private String generateRandomPin() {
@@ -18,8 +36,8 @@ public class GameLobby {
         return String.format("%05d", num);
     }
 
-    public String getGamePin() {
-        return gamePin;
-    }
+    //public String getGamePin() {
+    //    return gamePin;
+    //}
 
 }
