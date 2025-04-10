1229f93bf02fc9a27558f84ee349a91eee810eb8
Harald <haraldwangsvik@gmail.com>
2025-04-10 20:30:20 +0200

    change game view

--- core/src/main/java/group15/gdx/project/controller/GameController.java
+++ core/src/main/java/group15/gdx/project/controller/GameController.java
@@ -135,4 +135,12 @@ public class GameController {
         }
         return null;
     }
+
+    public int getCurrentScore(String activePlayerName) {
+        Player player = findPlayer(activePlayerName);
+        if (player != null) {
+            return player.getScore();
+        }
+        return 0;
+    }
 }
